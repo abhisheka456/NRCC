c0450f7322aad36a6de6595e10367055a16dcfd3
Abhishek Kumar <47485978+abhisheka456@users.noreply.github.com>
2024-03-14 15:18:40 +0900

    Add files via upload

---
+++ experiment/BYOL_s/cifar_10/cifar10_save_feature.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+now=$(date +"%Y%m%d_%H%M%S")
+job='cifar10'
+ROOT=../../..
+
+mkdir -p log
+
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+# use torch.distributed.launch
+python -m torch.distributed.launch \
+    --nproc_per_node=1 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=localhost \
+    --master_port=17672 \
+    $ROOT/main_save.py cifar10_r18_byol_s_save_feature.yml 2>&1 | tee log/seg_$now.txt
+
+
+# python -m torch.distributed.launch --nproc_per_node=4 --master_port=17672 main.py config/cifar10_r18_psclr2.yml
---
+++ experiment/BYOL_s/cifar_10/run_n_times.sh
@@ -0,0 +1,22 @@
+sh cifar10.sh 4 57067
+sh cifar10.sh 4 57067
+sh cifar10.sh 4 57067
+sh cifar10.sh 4 57067
+sh cifar10.sh 4 57067
+sh cifar10.sh 4 57067
+sh cifar10.sh 4 57067
+sh cifar10.sh 4 57067
+sh cifar10.sh 4 57067
+sh cifar10.sh 4 57067
+
+# sh cifar10.sh 4 57068
+# sh cifar10.sh 4 57068
+# sh cifar10.sh 4 57068
+# sh cifar10.sh 4 57068
+# sh cifar10.sh 4 57068
+# sh cifar10.sh 4 57068
+# sh cifar10.sh 4 57068
+# sh cifar10.sh 4 57068
+# sh cifar10.sh 4 57068
+# sh cifar10.sh 4 57068
+
