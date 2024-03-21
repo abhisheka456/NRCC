aa5816d840bf8f35b24699c796592ab38dfed6d2
Abhishek Kumar <47485978+abhisheka456@users.noreply.github.com>
2024-03-21 21:45:39 +0900

    Update byol.py

--- models/byol_sghm/byol.py
+++ models/byol_sghm/byol.py
@@ -1,12 +1,3 @@
-# -*- coding: UTF-8 -*-
-'''
-@Project : ProPos 
-@File    : byol.py
-@Author  : Zhizhong Huang from Fudan University
-@Homepage: https://hzzone.github.io/
-@Email   : zzhuang19@fudan.edu.cn
-@Date    : 2022/10/19 9:26 PM 
-'''
 
 import argparse
 import copy
